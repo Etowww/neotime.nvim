d3f726eae3f564b7b25b3a2cb4a9427380af68f2
Etowww <etow.engineering@gmail.com>
2025-04-29 00:01:04 -0700

    Testing function / UI initial Setup

---
+++ lua/neotime/init.lua
@@ -0,0 +1,13 @@
+-- /lua/neotime/init.lua created by DBTow
+
+local ui = require("neotime.ui")
+
+local M = {}
+
+function M.setup()
+	vim.api.nvim_create_user_command("NeotimeTest", function()
+		ui.test_window()
+	end, {})
+end
+
+return M
